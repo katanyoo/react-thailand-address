464ee9e51f6f26e2001c843376d26f84b2cbe367
Katanyoo Ubalee <ubalee.k@gmail.com>
2018-05-22 16:19:49 +0700

    unpublish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-exports.printMsg = function() {
-  console.log("this message from react-thailand-address")
+// @flow
+import React from 'react';
+
+class AddressForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (<div>
+      <div className="container">
+        <label className="label">label</label>
+      </div>
+    </div>);
+  }
 }
+
+export default AddressForm;
